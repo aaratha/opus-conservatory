ed09c6784ffddc24ad48482fd1cf2c3a9f6ad450
Aseem Ratha <aseemratha@gmail.com>
2026-09-15 17:12:18 -0400

    updates astro config to use www.aaratha.com address

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -15,7 +15,7 @@ const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aaratha.com',
+    site: 'https://www.aaratha.com',
     integrations: [
         mdx(),
         sitemap(),
